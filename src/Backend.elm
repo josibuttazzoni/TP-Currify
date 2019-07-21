5b699615fa0e5a0d9a614148f38ad0795460473a
josibuttazzoni <48684533+josibuttazzoni@users.noreply.github.com>
2019-07-21 20:41:41 -0300

    Funcion urlById

--- src/Backend.elm
+++ src/Backend.elm
@@ -20,7 +20,7 @@ import Models exposing (Model)
 
 -- Debería darnos la url de la cancion en base al id
 urlById : String -> List Song -> String
-urlById id songs = ""
+urlById id songs = (findById id songs).url
 
 -- Debería darnos las canciones que tengan ese texto en nombre o artista
 filterByName : String -> List Song -> List Song
